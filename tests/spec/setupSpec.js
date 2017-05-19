efeacc0862a9551f4b4f39605c76a40494e6b9d8
Bertold von Dormilich <Dormilich@netscape.net>
2017-05-19 15:51:16 +0200

    created expectation for AJAX url parameter.

--- tests/spec/setupSpec.js
+++ tests/spec/setupSpec.js
@@ -1,13 +1,32 @@
-describe('jQuery Request', function () {
+describe( 'jQuery Request', function () {
 
     beforeEach(function () {
-        jasmine.getFixtures().load('button-1.html');
+        jasmine.getFixtures().load( 'button-1.html' );
     });
 
-    it('should return the jQuery object', function () {
-        var $button = $('#button-1');
+    it( 'should return the jQuery object', function () {
+        var $button = $( '#button-1' );
         var obj = $button.request();
 
         expect( obj ).toBe( $button );
+    });
+
+    it( 'should read the AJAX url from explicit configuration', function () {
+        var $button = $( '#button-1' );
+        var testUrl = 'http://example.com/api?key=' + Math.random();
+        $button.request({
+            url: testUrl
+        });
+
+        expect( $button.request( 'option', 'url' ) ).toBe( testUrl );
+    });
+
+    it( 'should read the AJAX url from data-* configuration', function () {
+        var $button = $( '#button-1' );
+        var testUrl = $button.data( 'url' );
+
+        $button.request();
+
+        expect( $button.request( 'option', 'url' ) ).toBe( testUrl );
     });
 });
