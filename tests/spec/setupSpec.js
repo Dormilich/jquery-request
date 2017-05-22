af796678fed6e1a6f2d20cdfb9587543f4975e96
Bertold von Dormilich <Dormilich@netscape.net>
2017-05-22 12:10:29 +0200

    created expectations for the AJAX events.

--- tests/spec/setupSpec.js
+++ tests/spec/setupSpec.js
@@ -63,4 +63,112 @@ describe( 'jQuery Request', function () {
 
         expect( $button.request( 'option', 'method' ) ).toBe( 'HEAD' );
     });
+
+    describe( 'submit', function () {
+
+        beforeEach(function () {
+            jasmine.getFixtures().load('button-1.html');
+            jasmine.Ajax.install();
+        });
+
+        afterEach(function() {
+            jasmine.Ajax.uninstall();
+        });
+
+        it( 'should have set auto-submit disabled by default', function () {
+            var $button = $( '<button>click</button>' );
+
+            $button.request();
+
+            expect( $button.request( 'option', 'autoSubmit' ) ).toBe( false );
+        });
+
+        it( 'should emit a submit event before the AJAX request', function () {
+            jasmine.spyOnEvent( '#button-1', 'requestsubmit' );
+
+            var $button = $( '#button-1' ).request();
+
+            expect( 'requestsubmit' ).not.toHaveBeenTriggeredOn( '#button-1' );
+
+            $button.request( 'submit' );
+
+            expect( jasmine.Ajax.requests.mostRecent().url ).toBe( '/test' );
+
+            expect( 'requestsubmit' ).toHaveBeenTriggeredOn( '#button-1' );
+        });
+
+        it( 'should emit a success event after the AJAX request', function () {
+            jasmine.spyOnEvent( '#button-1', 'requesterror' );
+            jasmine.spyOnEvent( '#button-1', 'requestsuccess' );
+
+            $( '#button-1' ).request().request( 'submit' );
+
+            expect( 'requestsuccess' ).not.toHaveBeenTriggeredOn( '#button-1' );
+
+            jasmine.Ajax.requests.mostRecent().response({
+                status: 200,
+                contentType: 'text/plain',
+                responseText: 'successful response'
+            });
+
+            expect( 'requestsuccess' ).toHaveBeenTriggeredOn( '#button-1' );
+            expect( 'requesterror' ).not.toHaveBeenTriggeredOn( '#button-1' );
+        });
+
+        it( 'should receive the AJAX response as the success event', function () {
+            var done = jasmine.createSpy( 'done' );
+            jasmine.Ajax.stubRequest( '/test' ).andReturn({
+                status: 200,
+                contentType: 'text/plain',
+                responseText: 'successful request'
+            });
+
+            $( '#button-1' )
+                .on( 'requestsuccess', function ( evt ) {
+                    done( evt );
+                })
+                .request()
+                .request( 'submit' )
+            ;
+
+            expect( done ).toHaveBeenCalledWith( 'successful response' );
+        });
+
+        it( 'should emit an error event when the AJAX request fails', function () {
+            jasmine.spyOnEvent( '#button-1', 'requesterror' );
+            jasmine.spyOnEvent( '#button-1', 'requestsuccess' );
+
+            $( '#button-1' ).request().request( 'submit' );
+
+            expect( 'requesterror' ).not.toHaveBeenTriggeredOn( '#button-1' );
+
+            jasmine.Ajax.requests.mostRecent().response({
+                status: 400,
+                contentType: 'text/plain',
+                responseText: 'error message'
+            });
+
+            expect( 'requesterror' ).toHaveBeenTriggeredOn( '#button-1' );
+            expect( 'requestsuccess' ).not.toHaveBeenTriggeredOn( '#button-1' );
+        });
+
+        it( 'should receive the AJAX object as the error event', function () {
+            var done = jasmine.createSpy( 'done' );
+            jasmine.Ajax.stubRequest( '/test' ).andReturn({
+                status: 400,
+                contentType: 'text/plain',
+                responseText: 'error request'
+            });
+
+            $( '#button-1' )
+                .on( 'requesterror', function ( evt ) {
+                    done( evt );
+                })
+                .request()
+                .request( 'submit' )
+            ;
+
+            expect( done ).toHaveBeenCalledWith( jasmine.Ajax.requests.mostRecent() );
+        });
+    });
 });
