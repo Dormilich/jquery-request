2ee4ac86f6bf05083bb7a7a6904b95c2d1b98603
Bertold von Dormilich <Dormilich@netscape.net>
2017-06-17 10:11:03 +0200

    reduced test set to the submit event only.

--- tests/spec/setupSpec.js
+++ tests/spec/setupSpec.js
@@ -5,7 +5,7 @@ describe( 'jQuery Request', function () {
         var obj = $button.request();
 
         expect( obj ).toBe( $button );
-    });
+    } );
 
     it( 'should read the AJAX url from explicit configuration', function () {
         var $button = $( '<button>click</button>' );
@@ -15,7 +15,7 @@ describe( 'jQuery Request', function () {
         });
 
         expect( $button.request( 'option', 'url' ) ).toBe( testUrl );
-    });
+    } );
 
     it( 'should read the AJAX url from data-* configuration', function () {
         var $button = $( '<button data-url="/test">click</button>' );
@@ -24,7 +24,7 @@ describe( 'jQuery Request', function () {
         $button.request();
 
         expect( $button.request( 'option', 'url' ) ).toBe( testUrl );
-    });
+    } );
 
     it( 'should overwrite explicit settings with data-* settings', function () {
         var $button = $( '<button data-url="/test">click</button>' );
@@ -35,7 +35,7 @@ describe( 'jQuery Request', function () {
         });
 
         expect( $button.request( 'option', 'url' ) ).toBe( dataUrl );
-    });
+    } );
 
     it( 'should have a default AJAX method', function () {
         var $button = $( '<button>click</button>' );
@@ -43,7 +43,7 @@ describe( 'jQuery Request', function () {
         $button.request();
 
         expect( $button.request( 'option', 'method' ) ).toBe( 'GET' );
-    });
+    } );
 
     it( 'should read the AJAX method from explicit configuration', function () {
         var $button = $( '<button>click</button>' );
@@ -53,7 +53,7 @@ describe( 'jQuery Request', function () {
         });
 
         expect( $button.request( 'option', 'method' ) ).toBe( 'POST' );
-    });
+    } );
 
     it( 'should read the AJAX method from data-* configuration', function () {
         var $button = $( '<button data-method="HEAD">click</button>' );
@@ -62,113 +62,69 @@ describe( 'jQuery Request', function () {
         $button.request();
 
         expect( $button.request( 'option', 'method' ) ).toBe( 'HEAD' );
-    });
+    } );
 
     describe( 'submit', function () {
 
-        beforeEach(function () {
-            jasmine.getFixtures().load('button-1.html');
+        beforeEach( function () {
+            jasmine.getFixtures().load( 'button-1.html' );
             jasmine.Ajax.install();
-        });
+        } );
 
-        afterEach(function() {
+        afterEach( function () {
             jasmine.Ajax.uninstall();
-        });
+        } );
+
+        it( 'should fail without a target URL', function () {
+            var $button = $( '<button>never submit</button>' );
+            var fail = jasmine.createSpy( 'fail' );
+
+            $button
+                .request()
+                .request( 'submit' )
+                .fail( fail )
+            ;
+
+            expect( $button.request( 'option', 'url' ) ).toBe( false );
+            expect( fail ).toHaveBeenCalledWith( 'Missing required AJAX URL.' );
+        } );
 
         it( 'should have set auto-submit disabled by default', function () {
-            var $button = $( '<button>click</button>' );
+            var $button = $( '#button-1' );
+
+            jasmine.spyOnEvent( '#button-1', 'requestsubmit' );
 
             $button.request();
 
             expect( $button.request( 'option', 'autoSubmit' ) ).toBe( false );
-        });
+            expect( $button.request( 'option', 'url' ) ).toBe( '/test' );
+            expect( 'requestsubmit' ).not.toHaveBeenTriggeredOn( '#button-1' );
+        } );
 
         it( 'should emit a submit event before the AJAX request', function () {
-            jasmine.spyOnEvent( '#button-1', 'requestsubmit' );
-
             var $button = $( '#button-1' ).request();
+
+            jasmine.spyOnEvent( '#button-1', 'requestsubmit' );
 
             expect( 'requestsubmit' ).not.toHaveBeenTriggeredOn( '#button-1' );
 
             $button.request( 'submit' );
 
-            expect( jasmine.Ajax.requests.mostRecent().url ).toBe( '/test' );
+            expect( jasmine.Ajax.requests.mostRecent().url ).toBe( '/test?id=42' );
 
             expect( 'requestsubmit' ).toHaveBeenTriggeredOn( '#button-1' );
-        });
+        } );
 
-        it( 'should emit a success event after the AJAX request', function () {
-            jasmine.spyOnEvent( '#button-1', 'requesterror' );
-            jasmine.spyOnEvent( '#button-1', 'requestsuccess' );
+        it( 'should fire on initialisation when auto-submit is set', function () {
+            jasmine.spyOnEvent( '#button-1', 'requestsubmit' );
 
-            $( '#button-1' ).request().request( 'submit' );
-
-            expect( 'requestsuccess' ).not.toHaveBeenTriggeredOn( '#button-1' );
-
-            jasmine.Ajax.requests.mostRecent().response({
-                status: 200,
-                contentType: 'text/plain',
-                responseText: 'successful response'
+            $( '#button-1' ).request({
+                autoSubmit: true
             });
 
-            expect( 'requestsuccess' ).toHaveBeenTriggeredOn( '#button-1' );
-            expect( 'requesterror' ).not.toHaveBeenTriggeredOn( '#button-1' );
-        });
+            expect( 'requestsubmit' ).toHaveBeenTriggeredOn( '#button-1' );
+        } );
 
-        it( 'should receive the AJAX response as the success event', function () {
-            var done = jasmine.createSpy( 'done' );
-            jasmine.Ajax.stubRequest( '/test' ).andReturn({
-                status: 200,
-                contentType: 'text/plain',
-                responseText: 'successful request'
-            });
+    } );
 
-            $( '#button-1' )
-                .on( 'requestsuccess', function ( evt ) {
-                    done( evt );
-                })
-                .request()
-                .request( 'submit' )
-            ;
-
-            expect( done ).toHaveBeenCalledWith( 'successful response' );
-        });
-
-        it( 'should emit an error event when the AJAX request fails', function () {
-            jasmine.spyOnEvent( '#button-1', 'requesterror' );
-            jasmine.spyOnEvent( '#button-1', 'requestsuccess' );
-
-            $( '#button-1' ).request().request( 'submit' );
-
-            expect( 'requesterror' ).not.toHaveBeenTriggeredOn( '#button-1' );
-
-            jasmine.Ajax.requests.mostRecent().response({
-                status: 400,
-                contentType: 'text/plain',
-                responseText: 'error message'
-            });
-
-            expect( 'requesterror' ).toHaveBeenTriggeredOn( '#button-1' );
-            expect( 'requestsuccess' ).not.toHaveBeenTriggeredOn( '#button-1' );
-        });
-
-        it( 'should receive the AJAX object as the error event', function () {
-            var done = jasmine.createSpy( 'done' );
-            jasmine.Ajax.stubRequest( '/test' ).andReturn({
-                status: 400,
-                contentType: 'text/plain',
-                responseText: 'error request'
-            });
-
-            $( '#button-1' )
-                .on( 'requesterror', function ( evt ) {
-                    done( evt );
-                })
-                .request()
-                .request( 'submit' )
-            ;
-
-            expect( done ).toHaveBeenCalledWith( jasmine.Ajax.requests.mostRecent() );
-        });
-    });
-});
+} );
